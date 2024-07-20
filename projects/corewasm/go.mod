8b59b15210223bff208967b5ed607b204ac58df2
Steven Zack <zigzigcheers@gmail.com>
2024-07-20 17:33:35 +0300

    wasm

---
+++ projects/corewasm/go.mod
@@ -0,0 +1,7 @@
+module corewasm
+
+go 1.22.5
+
+replace core => ../core
+
+require core v0.0.0-00010101000000-000000000000
--- projects/editfile/src/App.tsx
+++ projects/editfile/src/App.tsx
@@ -1,8 +1,14 @@
-import type { Component } from 'solid-js';
+import { createSignal, type Component } from 'solid-js';
 
 const App: Component = () => {
+  const [text,setText]=createSignal('text')
+
   return <div>
     <h1>Edit File</h1>
+    <span>{text()}</span>
+    <button onclick={()=>{
+      setText(eval('add(1,2,3)'))
+    }}>Do ADD</button>
   </div>;
 };
 
